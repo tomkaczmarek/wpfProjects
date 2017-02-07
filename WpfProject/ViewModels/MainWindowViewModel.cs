06a528b6b6ab9bb66f4215b103827c750cffad3f
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-07 21:47:04 +0100

    First mvvm

---
+++ WpfProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfProject.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly string _defaultName = "Podaj imię";
+        private readonly string _defaultSurName = "Podaj nazwisko";
+        private string _name, _surName, _result;
+
+        public ICommand ValidateCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ValidateCommand = new DelegateCommand(ValidateAction);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(_name) ? _name : _defaultName;
+            }
+            set
+            {
+                _name = value == string.Empty ? _defaultName : value;
+                OnPropertyChanged(() => Name);
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(_surName) ? _surName : _defaultSurName;
+            }
+            set
+            {
+                _surName = value == string.Empty ? _defaultSurName : value;
+                OnPropertyChanged(() => Surname);
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                _result = value;
+                OnPropertyChanged(() => Result);
+            }
+        }
+
+        private void ValidateAction()
+        {
+            bool validationResult = Name != _defaultName && Surname != _defaultSurName;
+            if(!validationResult)
+            {
+                Result = "Błąd";
+                return;
+            }
+            Result = "OK";
+        }
+    }
+}
