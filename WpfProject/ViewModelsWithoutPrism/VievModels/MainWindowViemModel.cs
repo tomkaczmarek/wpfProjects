dbb6153bc4f28b3b428e64c22827475813174ef7
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-08 17:36:30 +0100

    MVVM - callerMember .NET 4,5 and LambdaExpression to take propertyName

---
+++ WpfProject/ViewModelsWithoutPrism/VievModels/MainWindowViemModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfProject.ViewModelsWithoutPrism.VievModels
+{
+    public class MainWindowViemModel : WindowBaseProperty
+    {
+        private string _name;
+        private string _name2;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(() => Name);
+            }
+        }
+
+        public string Name2
+        {
+            get { return _name2; }
+            set
+            {
+                _name2 = value;
+                OnPropertyChangedCallerMember();
+            }
+        }
+    }
+}
---
+++ WpfProject/ViewModelsWithoutPrism/VievModels/WindowBaseProperty.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace WpfProject.ViewModelsWithoutPrism.VievModels
+{
+    public class WindowBaseProperty : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void PropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected string GetPropertyName<TProperty>(Expression<Func<TProperty>> property)
+        {
+            var lambda = (LambdaExpression)property;
+
+            MemberExpression memberExpression;
+            if(lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+                memberExpression = (MemberExpression)lambda.Body;
+
+            return memberExpression.Member.Name;
+        }
+
+        public void OnPropertyChanged<TProperty>(Expression<Func<TProperty>> property)
+        {
+            PropertyChange(GetPropertyName(property));
+        }
+
+        public void OnPropertyChangedCallerMember([CallerMemberName] string name="")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
